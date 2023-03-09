13fbba076017724da91649ab7a9c262c03ad5b12
tenra <tetenchi@yahoo.co.jp>
2023-03-09 09:06:53 +0900

    header userinfo

--- src/core/layouts/Header.tsx
+++ src/core/layouts/Header.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Link from "next/link"
 import { Routes } from "@blitzjs/next"
 import logout from "src/auth/mutations/logout"
 import { useMutation } from "@blitzjs/rpc"
+import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 
-export const Header: React.FC = () => {
+const UserInfo = () => {
+  const currentUser = useCurrentUser()
+
+  return (
+    <>
+      <Link href={`/users/${currentUser?.id}`}>
+        {currentUser?.name}
+      </Link>/
+    </>
+  )
+}
+
+const Header = () => {
   const [logoutMutation] = useMutation(logout)
 
   const handleLogout = async () => {
@@ -14,11 +27,15 @@ export const Header: React.FC = () => {
   return (
     <header>
       <Link href={Routes.Home()}>homepage</Link>/
-      <Link href={Routes.UsersPage()}>UsersPage</Link>/
       <Link href={Routes.PromotionsPage()}>PromotionsPage</Link>/
+      <Link href={Routes.UsersPage()}>UsersPage</Link>/
       <button onClick={handleLogout} className="bg-green-300">
         Logout
-      </button>
+      </button>/
+      <Suspense fallback="">
+        <UserInfo />
+      </Suspense>
     </header>
   )
 }
+export default Header
